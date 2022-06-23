75e6a21f842349fa16d644bc6d54585567b56841
Aleksei Gutikov <gutikoff@gmail.com>
2022-06-23 22:31:03 +0300

    gocc list

--- lispy/parser/ast/ast.go
+++ lispy/parser/ast/ast.go
@@ -12,26 +12,53 @@ type Atom struct {
 	Value string
 }
 
+type Sexpr struct {
+	Value interface{}
+}
+
 type List struct {
-	List []Atom
+	List []Sexpr
+}
+
+type Sequence struct {
+	List []Sexpr
 }
 
 func NewAtom(id Attrib) (*Atom, error) {
 	return &Atom{string(id.(*token.Token).Lit)}, nil
 }
 
-func (this *Atom) String() string {
-	return this.Value
+func NewSequence(s Attrib) (*Sequence, error) {
+	sexpr := *s.(*Sexpr)
+	return &Sequence{[]Sexpr{sexpr}}, nil
 }
 
-func NewList(head Attrib, tail Attrib) (*List, error) {
-	atom := tail.(*Atom)
-	if head == nil {
-		return &List{List: []Atom{*atom}}, nil
+func Cons(car Attrib, cdr Attrib) (*Sequence, error) {
+	sexpr := *car.(*Sexpr)
+	seq := *cdr.(*Sequence)
+
+	seq.List = append([]Sexpr{sexpr}, seq.List...)
+
+	return &seq, nil
+}
+
+func NewList(seq Attrib) (*List, error) {
+	lst := List{}
+	if seq != nil {
+		lst.List = seq.(*Sequence).List
 	}
-	s := head.(*List)
-	s.List = append(s.List, *atom)
-	return s, nil
+	return &lst, nil
+}
+
+func NewSexpr(s Attrib) (*Sexpr, error) {
+	switch s := s.(type) {
+	case *Atom:
+		return &Sexpr{*s}, nil
+	case *List:
+		return &Sexpr{*s}, nil
+	default:
+		panic("Invalid NewSexpr() argument")
+	}
 }
 
 func Map[From any, To any](f func(From) To, args []From) []To {
@@ -42,6 +69,25 @@ func Map[From any, To any](f func(From) To, args []From) []To {
 	return r
 }
 
+func (this *Atom) String() string {
+	return this.Value
+}
+
 func (this *List) String() string {
-	return strings.Join(Map(func(a Atom) string { return a.String() }, this.List), " ")
+	return "( " + strings.Join(Map(func(a Sexpr) string { return a.String() }, this.List), " ") + " )"
+}
+
+func (this *Sequence) String() string {
+	return strings.Join(Map(func(a Sexpr) string { return a.String() }, this.List), " ")
+}
+
+func (this *Sexpr) String() string {
+	switch s := this.Value.(type) {
+	case Atom:
+		return s.String()
+	case List:
+		return s.String()
+	default:
+		panic("Invalid Sexpr")
+	}
 }
